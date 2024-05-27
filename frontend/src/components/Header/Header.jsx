1a88efc182b4c6028912df6182baaa374e8be34d
Ankit8210 <ankitsinha25061909@gmail.com>
2024-05-27 07:45:44 +0530

    HeaderSection Complete

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order Your Favourite Food here</h2>
+                <p>Food is any substance consumed to provide nutritional support and energy to an organism. It can be raw, processed, or formulated and is consumed orally by animals for growth, health, or pleasure. </p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
