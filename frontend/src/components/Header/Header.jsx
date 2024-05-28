3bb3217a9121916402fdcb0f659899f448dc64b9
Ankit8210 <ankitsinha25061909@gmail.com>
2024-05-28 09:46:15 +0530

    ExoloreMenu and Food Display Done

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className='header'>
             <div className="header-contents">
                 <h2>Order Your Favourite Food here</h2>
-                <p>Food is any substance consumed to provide nutritional support and energy to an organism. It can be raw, processed, or formulated and is consumed orally by animals for growth, health, or pleasure. </p>
+                <p>Food is any substance consumed to provide nutritional support and energy to an organism. It can be raw, processed, or formulated and is consumed orally by animals for growth, health, or pleasure. Food is mainly composed of water, lipids, proteins, and carbohydrates.</p>
                 <button>View Menu</button>
             </div>
         </div>
