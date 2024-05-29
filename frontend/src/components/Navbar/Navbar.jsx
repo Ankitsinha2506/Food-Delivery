2e7ee0aa3166955560828791b31b8883fdcb68e7
Ankit8210 <ankitsinha25061909@gmail.com>
2024-05-29 23:07:12 +0530

    Login & SignUp Layout completed.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 import {assets} from '../../assets/assets'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("Home")
   return (
@@ -22,7 +22,7 @@ const Navbar = () => {
                 <img src= {assets.basket_icon} alt='basketIcon'/>
                 <div className="dot"></div>
             </div>
-            <button>Sign In</button>
+            <button onClick={()=>setShowLogin(true)}>Sign In</button>
         </div>
         
       
